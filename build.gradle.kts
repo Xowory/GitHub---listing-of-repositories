2c6d83220c551e9972c0b8d81dcaf67f514efeda
Xowory <95762263+Xowory@users.noreply.github.com>
2021-12-30 14:14:37 +0100

    Fix build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,26 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+	repositories {
+		mavenCentral()
+		jcenter()
+	}
+
+	dependencies {
+		classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+	}
+}
+
 plugins {
 	id("org.springframework.boot") version "2.6.2"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
+	id ("application")
+}
+
+application{
+	mainClass
 }
 
 group = "io.github.Xowory"
